6bc5e40b3f09429b699fef30eada1a3b989a4f6c
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2017-09-11 12:54:57 -0300

    segundo commit

--- src/polinomio/Polinomio.java
+++ src/polinomio/Polinomio.java
@@ -1,10 +1,72 @@
 package polinomio;
 
 public class Polinomio {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 System.out.println("hola mundo");
+	
+	private int grado ;
+	private double[] coeficientes;
+	
+	public Polinomio(String path){
+		
+		
+		
+	}
+	
+	public double evaluarMSucesivas (double x ) {  
+		
+		
+		
+		double resultado=0.0 ;
+		
+		return resultado ;
+		
+	}
+	
+	public double evaluarRecursiva (double x ) { 
+		
+		double resultado=0.0 ;
+		
+		return resultado ; 
+	}   
+	
+	public double evaluarRecursivaPar (double x ) { 
+		
+		double resultado=0.0 ;
+		
+		return resultado ;
+		
+	}
+	
+	public double evaluarProgDinamica (double x ) {
+		
+		double resultado=0.0 ;
+		
+		return resultado ;
+		
+	}
+	
+	public double evaluarMejorada (double x ) { 
+		
+		double resultado=0.0 ;
+		
+		return resultado ;
+		
+	}
+	
+	public double evaluarPow (double x ) {
+		
+		double resultado=0.0 ;
+		
+		return resultado ;
+		
+	}
+	
+	// y a s u g e r e n c i a de Lucas P
+	public double evaluarHorner ( double x ) { 
+		
+		double resultado=0.0 ;
+		
+		return resultado ;
+		
 	}
 
 }
