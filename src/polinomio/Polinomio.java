97d6500a3d78fbcd9137e60707cde54efe35db4e
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2017-09-11 13:45:33 -0300

    evaluarMSucesivas

--- src/polinomio/Polinomio.java
+++ src/polinomio/Polinomio.java
@@ -5,19 +5,27 @@ public class Polinomio {
 	private int grado ;
 	private double[] coeficientes;
 	
-	public Polinomio(String path){
+	public Polinomio(int grado, double[] coeficientes){
 		
-		
-		
+		this.coeficientes=new double[grado+1]; 
+		this.coeficientes=coeficientes;
+		this.grado=grado;
 	}
 	
 	public double evaluarMSucesivas (double x ) {  
 		
+		int longitud= this.grado;
+		double acum=0;
+		double auxPotencia=1;
+		for(int i=0;i<=longitud;i++) {
+			for(int j=0;j<longitud-i;j++) 
+				auxPotencia*= x;
+			auxPotencia*=(this.coeficientes[i]);
+			acum += auxPotencia;
+			auxPotencia=1;
+		}	
 		
-		
-		double resultado=0.0 ;
-		
-		return resultado ;
+		return acum ;
 		
 	}
 	
@@ -67,6 +75,18 @@ public class Polinomio {
 		
 		return resultado ;
 		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int grado=2;
+		double[] coef= new double[] {1,2,3};
+		Polinomio pol=new Polinomio(grado, coef);
+		double res=pol.evaluarMSucesivas(2);
+		System.out.println(res);
 	}
 
 }
